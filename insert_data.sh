b30207306d5b2a6251110a59f3983b1874d06921
Nicolas Vo <nicolasvo.jc@gmail.com>
2024-11-28 07:41:30 +0000

    feat: Initial commit
    Begin insert_data pseudo code

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,23 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
+echo "$($PSQL "TRUNCATE TABLE teams,games")"
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WIN_GOALS OP_GOALS
+do
+  # guard clause to skip first line
+  if [[ $YEAR == 'year' ]]
+  then
+    continue
+  fi
+
+  # query if winner_id in teams
+
+  # if no winner_id
+
+  # insert winner into teams 
+
+  # query winner_id
+  
+done
