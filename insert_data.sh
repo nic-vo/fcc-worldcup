5b444d24e2a8f751f1a091050b31f2465382bc14
Nicolas Vo <nicolasvo.jc@gmail.com>
2024-11-28 23:58:33 +0000

    feat: Finish insert_data and prototype queries.sh
    TODO: fix the fourth last query

--- insert_data.sh
+++ insert_data.sh
@@ -19,14 +19,29 @@ do
     continue
   fi
   # query if winner_id in teams
-  WINNER_ID="$($PSQL "SELECT name FROM teams WHERE name='$WINNER'")"
+  WINNER_ID="$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")"
   # if no winner_id
   if [[ -z $WINNER_ID ]]
   then
+    # insert winner into teams 
     echo "$($PSQL "INSERT INTO teams(name) VALUES('$WINNER');"): $WINNER"
   fi
-  # insert winner into teams 
-
   # query winner_id
+  WINNER_ID="$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")"
+  
+  # query if winner_id in teams
+  OPPONENT_ID="$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")"
+  # if no OPPONENT_id
+  if [[ -z $OPPONENT_ID ]]
+  then
+    # insert OPPONENT into teams 
+    echo "$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT');"): $OPPONENT"
+  fi
+  # query OPPONENT_id
+  OPPONENT_ID="$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")"
+
+  echo "$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WIN_GOALS, $OP_GOALS)"): $YEAR, $ROUND, $WINNER_ID vs $OPPONENT_ID -- $WIN_GOALS : $OP_GOALS"
   
 done
+
+echo "$($PSQL "SELECT COUNT(*) FROM teams;")"
